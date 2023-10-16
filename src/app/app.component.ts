d21d47302e0d28dadcc70de815e6746855ccbb64
lancasmx <angelprep92@gmail.com>
2023-10-15 20:14:24 -0600

    Se agregaron vinculos a las imagenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngulasCursoBasico';
+  name = 'Angel';
+  age = 31;
+  img = 'https://img.freepik.com/foto-gratis/lobo-colorido-fondo-negro_1340-39237.jpg?size=626&ext=jpg&ga=GA1.1.1016474677.1697155200&semt=ais';
+  img2 = 'https://scontent.fmex10-1.fna.fbcdn.net/v/t39.30808-6/387056014_738048151670722_3299078430535870773_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=vTd-PTsp9PcAX9A4rbV&_nc_ht=scontent.fmex10-1.fna&oh=00_AfAC6w_TWIefNwLTYEYEu0I_pPsi1IlJbEll7SjctgUToA&oe=65324EFB';
+  img3 = 'https://scontent.fmex10-2.fna.fbcdn.net/v/t39.30808-6/392940567_741389274669943_4240576983403286201_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=uXVO7l2vl2YAX8YYLyT&_nc_ht=scontent.fmex10-2.fna&oh=00_AfDd1-Guf6T7JFNcXNQdUXB-0e4rF_W4qC-o8--Q9z70Qw&oe=65317DD1';
+  img4 = 'https://scontent.fmex10-4.fna.fbcdn.net/v/t39.30808-6/392822028_741389528003251_4812483744013871862_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=4gIKu1O2LwMAX-kIdde&_nc_ht=scontent.fmex10-4.fna&oh=00_AfCfSmZ9R9IZz6sTeppzvQPxIF9I7sM7YwcOuEjtXtT3nQ&oe=65309572';
+
 }
